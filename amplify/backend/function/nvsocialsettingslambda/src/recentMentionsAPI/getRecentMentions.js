75050ee216e1dbf968ad1e1c4ce4ee8bc0473880
leesebas <leesebas@amazon.com>
2021-09-07 10:06:51 +0800

    reduce the number of records query

--- amplify/backend/function/nvsocialsettingslambda/src/recentMentionsAPI/getRecentMentions.js
+++ amplify/backend/function/nvsocialsettingslambda/src/recentMentionsAPI/getRecentMentions.js
@@ -80,7 +80,7 @@ const executeCommand = async (command, callback=async (resp)=> resp ) => {
 const filterByCurrMonth = (contents) => {
     return contents
         .filter((res) => {
-            return moment().diff(moment(res.LastModified), 'months') === 0
+            return moment().diff(moment(res.LastModified), 'days') < 7
         })
 }
 
