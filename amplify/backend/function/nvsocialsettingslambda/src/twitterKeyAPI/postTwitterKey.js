416450f30bf17e7d641174390c4076a9d0fae006
Yudho Ahmad Diponegoro <diponego@amazon.com>
2021-09-16 02:00:18 +0000

    added getting lambda arn dynamically

--- amplify/backend/function/nvsocialsettingslambda/src/twitterKeyAPI/postTwitterKey.js
+++ amplify/backend/function/nvsocialsettingslambda/src/twitterKeyAPI/postTwitterKey.js
@@ -1,13 +1,19 @@
 const { SecretsManagerClient, PutSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
 
 const secretManagerClient = new SecretsManagerClient({
+    region: "ap-southeast-1"
+});
+
+const ssmClient = new SSMClient({
     region: "ap-southeast-1"
 });
 
 const lambdaClient = new LambdaClient({
     region: "ap-southeast-1"
 });
+const lambdaARNParameterStoreName = "TW_LAMBDA_ARN"
 
 const postTwitterKey = async (secretString) => {
 
@@ -17,7 +23,12 @@ const postTwitterKey = async (secretString) => {
     });
     await secretManagerClient.send(putSecretValueCommand);
 
-    const lambdaARN = process.env.TW_ACTIVATE_TOKEN_LAMBDA_ARN
+    const getParameterCommand = new GetParameterCommand({
+        "Name": `${lambdaARNParameterStoreName}`,
+        "WithDecryption": true
+    })
+    const ssmResponse = await ssmClient.send(getParameterCommand);
+    const lambdaARN = ssmResponse.Parameter.Value;
     const invokeCommand = new InvokeCommand({
         FunctionName: lambdaARN,
         LogType: "Tail"
