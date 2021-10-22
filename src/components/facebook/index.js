cb5ccd49ee7776ca3bd458dc14510268a8c98acd
Yudho Ahmad Diponegoro <diponego@amazon.com>
2021-10-22 10:25:36 +0000

    added back instagram permission

--- src/components/facebook/index.js
+++ src/components/facebook/index.js
@@ -197,7 +197,7 @@ export default function Facebook() {
                             <div className="fb-login-button" data-width="" data-size="large"
                                  data-button-type="continue_with" data-onlogin="handleOnFbLogin()"
                                  data-layout="default" data-auto-logout-link="true" data-use-continue-as="false"
-                                 data-scope="pages_manage_metadata,pages_manage_engagement,pages_show_list,pages_read_user_content"
+                                 data-scope="pages_manage_metadata,pages_manage_engagement,pages_show_list,pages_read_user_content,instagram_manage_comments,instagram_basic"
                             />
                         </div>
                         <div className={classes.fbActionButtonContainer}>
