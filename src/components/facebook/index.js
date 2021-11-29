ccc272634f4e7d9229c7aaa8da9b2df21f2cccfc
Yudho Ahmad Diponegoro <diponego@amazon.com>
2021-11-29 07:48:27 +0000

    added all permissions

--- src/components/facebook/index.js
+++ src/components/facebook/index.js
@@ -232,7 +232,7 @@ export default function Facebook() {
                             <div className="fb-login-button" data-width="" data-size="large"
                                  data-button-type="continue_with" data-onlogin="handleOnFbLogin()"
                                  data-layout="default" data-auto-logout-link="true" data-use-continue-as="false"
-                                 data-scope="pages_manage_metadata,pages_show_list,pages_read_user_content"
+                                 data-scope="pages_manage_metadata,pages_show_list,pages_read_user_content,instagram_manage_comments,instagram_basic,pages_manage_engagement"
                             />
                         </div>
                         <div className={classes.fbActionButtonContainer}>
